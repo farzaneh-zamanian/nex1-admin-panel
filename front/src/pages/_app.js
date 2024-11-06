47c3dc5dafcf6116854b45554cd7b73e0efbc31d
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-11-06 11:24:03 +0330

    define floder sttructure

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.scss";
 import "@/styles/fonts.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,6 +1,9 @@
+import styles from "../styles/Home.module.scss"
 
 export default function Home() {
   return (
- <></>
+ <main className={styles.containerMain}>
+  <h1>botostart</h1>
+ </main>
   );
 }
