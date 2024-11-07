cc12f6bf789f983b20021832fc449c862cc4b822
Farzaneh Zamanian <152534852+farzaneh-zamanian@users.noreply.github.com>
2024-11-07 13:28:10 +0330

    install axios and config that for fetching data in adminPage

--- front/src/pages/admin/index.js
+++ front/src/pages/admin/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AdminPage from '../../../components/templates/AdminPage/AdminPage'
+import { api } from '../../../configs/api';
 
 function adminIndex({ products }) {
   return (
@@ -12,14 +13,21 @@ export default adminIndex
 
 //FETCH DATA FROM API
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3001/products");
-  const data = await res.json();
-  return {
-    props: { products: data.data },
-    revalidate: 10 //seconds
-  }
+export async function getServerSideProps() {
+  try {
+    const res = await api.get('/products');
+    const data = res.data;
+    return {
+      props: { products: data.data },
+ 
+    };
+  } catch (error) {
+    console.error('Error fetching products:', error.message);
 
+    return {
+      props: { products: [] },
+    };
+  }
 }
 
 
